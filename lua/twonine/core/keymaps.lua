e620368178d4d869309d3e528df7a8d286e4884d
tn <1185366392@qq.com>
2023-02-26 13:48:15 +0800

    first commit

---
+++ lua/twonine/core/keymaps.lua
@@ -0,0 +1,109 @@
+-- set leader key to space
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+-- neovim设置快捷键方式(官网)
+local map = vim.api.nvim_set_keymap
+local opt = {noremap = true,silent=true}
+
+---------------------
+-- General Keymaps
+---------------------
+
+-- ---------- 视觉模式 ---------- ---
+-- 单行或多行移动
+
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Increment/decrement
+vim.keymap.set('n', '+', '<C-a>')
+vim.keymap.set('n', '-', '<C-x>')
+
+-- use jj to exit insert mode, jk recommended
+keymap.set("i", "jj", "<ESC>")
+
+-- clear search highlights
+keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+-- 切换buffer
+-- keymap.set("n", "<C-l>", ":bnext<CR>")
+-- keymap.set("n", "<C-h>", ":bprevious<CR>")
+
+-- Delete a word backwards
+vim.keymap.set('n', 'dw', 'vb"_d')
+
+-- 向上新起一行,向下新起一行
+map('i','<S-Home>',"<Esc>O",opt)
+map('i','<S-End>',"<Esc>o",opt)
+
+-- Insert mode with move
+map('i',"<C-k>","<Up>",opt)
+map('i',"<C-j>","<Down>",opt)
+map('i',"<C-l>","<Right>",opt)
+map('i',"<C-h>","<Left>",opt)
+
+-- Select all
+keymap.set('n', '<C-a>', 'gg<S-v>G')
+
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
+keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+
+keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
+
+-- 退出
+keymap.set('n', '<A-q>', ':q<CR>')
+map('n', 'w', ':w<CR>', opt)
+keymap.set('n', 'wq', ':wq<CR>')
+keymap.set('n', 'qq', ':qa!<CR>')
+
+----------------------
+-- Plugin Keybinds
+----------------------
+
+-- vim-maximizer
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle split window maximization
+
+-- nvim-tree
+keymap.set("n", "<A-m>", ":NvimTreeToggle<CR>") -- toggle file explorer
+
+
+-- telescope
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
+keymap.set("n", "<leader>lds", "<cmd>Telescope lsp_document_symbols<cr>") -- list all functions/structs/classes/modules in the current buffer
+
+-- telescope git commands
+keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git commits (use <cr> to checkout) ["gc" for git commits]
+keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>") -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
+keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>") -- list git branches (use <cr> to checkout) ["gb" for git branch]
+keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- list current changes per file with diff preview ["gs" for git status]
+
+-- hop cmd
+keymap.set("n", "<leader>hw", ":HopWord<cr>")
+keymap.set("n", "<leader>hww", ":HopWordMW<cr>")
+keymap.set("n", "<leader>hc", ":HopChar2<cr>")
+keymap.set("n", "<leader>hcc", ":HopChar2MW<cr>")
+keymap.set("n", "<leader>hl", ":HopLine<cr>")
+keymap.set("n", "<leader>hls", ":HopLineStart<cr>")
+
+-- restart lsp server
+keymap.set("n", "<leader>rs", ":LspRestart<CR>") -- mapping to restart lsp if necessary
+
+-- troubles
+vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>", { silent = true, noremap = true })
+vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", { silent = true, noremap = true })
+vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", { silent = true, noremap = true })
+vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", { silent = true, noremap = true })
+vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", { silent = true, noremap = true })
+vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", { silent = true, noremap = true })
