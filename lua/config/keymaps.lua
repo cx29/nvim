d619134030dacd76c8a614fa828029afc58fa38d
tn <1185366392@qq.com>
2023-09-17 18:27:23 +0800

    使用lazy初始化配置文件

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,28 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local Util = require("lazyvim.util")
+
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  -- 如果存在懒按键处理器就不要创建按键映射？
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
+map("n", "+", "<C-a>")
+map("n", "-", "<C-x>")
+
+map("i", "jk", "<ESC>")
+
+-- Select all
+map("n", "<C-a>", "gg<S-v>G")
+
+map("n", "w", ":w<CR>")
+map("n", "wq", ":wq<CR>")
+map("n", "qq", ":qa!<CR>")
---
+++ lua/plugins/notify.lua
@@ -0,0 +1,9 @@
+return {
+  "rcarriga/nvim-notify",
+  opts = {
+    stages = "slide",
+    timeout = 500,
+    minimum_width = 40,
+    max_width = 55,
+  },
+}
